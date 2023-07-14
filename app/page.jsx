2f1f8d592271bd842db576a9fd0a0baf3466fac6
FackJox <jackfox@tuta.io>
2023-07-14 21:05:01 +0100

    sorting css

--- app/page.jsx
+++ app/page.jsx
@@ -10,14 +10,9 @@ import useNavigation from '@/helpers/hooks/useNavigation'
 export default function Page() {
   const ref = useRef()
   usePlayAnimations(1)
-  const router = useRouter()
-
   useNavigation(ref, '/portfolio')
 
 
-  const handleClick = () => {
-    router.push('/about')
-  }
 
   return (
     <AnimatePresence>
@@ -26,25 +21,25 @@ export default function Page() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { delay: 2.5, duration: 2 } }}
         exit={{ opacity: 0, transition: { duration: 2.5 } }}
-        className='z-40 flex w-screen h-screen overflow-hidden bg-transparent text-icewhite'
+        className='z-40 flex items-center justify-center pb-10 w-screen h-screen bg-transparent text-icewhite'
       >
         <div className='grid grid-cols-3 grid-rows-2 gap-2'>
           <div className='col-span-3'></div>
           <Link href='/portfolio/stills' className=''>
-            <div className='row-start-2'>
+            <div className='row-start-2 m-10'>
               <p className='w-[200px] h-16 text-4xl text-center text-icewhite'>STILLS</p>
             </div>
           </Link>
           <Link href='/about' className=''>
           <div>
 
-            <div  className='row-start-2'>
+            <div  className='row-start-2 m-10'>
               <p className='w-[200px] h-16  text-5xl text-center text-gold'>ABOUT</p>
             </div>
           </div>
           </Link>
           <Link href='/portfolio/motion' className=''>
-            <div className='row-start-2'>
+            <div className='row-start-2 m-10'>
               <p className='w-[200px] h-12 text-4xl text-center text-icewhite'>MOTION</p>
             </div>
           </Link>
