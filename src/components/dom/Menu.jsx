cc24d2565babfbc0b4ef6f7aff8d657169fbfa97
FackJox <jackfox@tuta.io>
2023-07-19 15:23:59 +0100

    done 4 day

--- src/components/dom/Menu.jsx
+++ src/components/dom/Menu.jsx
@@ -17,9 +17,10 @@ export default function Menu() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { delay: 0, duration: 2 } }}
         exit={{ opacity: 0, transition: { duration: 2.5 } }}
-        className='ml-auto flex z-40 absolute top-16 right-12 justify-end bg-gray-800 text-icewhite'
+        className='ml-auto flex z-40 absolute top-16 right-8 justify-end bg-gray-800 text-icewhite menu'
         
       >
+        <div className='w-24 h-24' ></div>
         <div>
           <Link href='/portfolio/stills'>
             <p className='w-24 h-8 text-base text-right text-white'>STILLS</p>
