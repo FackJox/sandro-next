a1ac94f8531c07cc086b469951b7b02194fb2e42
FackJox <jackfox@tuta.io>
2023-10-02 15:47:18 +0100

    bare video styling

--- src/components/dom/Portfolio/Motion/MotionGallery.jsx
+++ src/components/dom/Portfolio/Motion/MotionGallery.jsx
@@ -26,8 +26,8 @@ export default function MotionGallery(props) {
     setSelectedPlaylistId,
     setPlaylistVideos,
   } = props
-    // console.log("🚀 ~ file: MotionGallery.jsx:28 ~ MotionGallery ~ videos:", videos)
-    // console.log("🚀 ~ file: MotionGallery.jsx:28 ~ MotionGallery ~ playlists:", playlists)
+  // console.log("🚀 ~ file: MotionGallery.jsx:28 ~ MotionGallery ~ videos:", videos)
+  // console.log("🚀 ~ file: MotionGallery.jsx:28 ~ MotionGallery ~ playlists:", playlists)
 
   const [activePlaylist, setActivePlaylist] = useState()
 
@@ -35,96 +35,104 @@ export default function MotionGallery(props) {
     console.log('activePlaylist', activePlaylist)
   }, [activePlaylist])
 
-function handleOnPlaylistClick(selectedPlaylistId) {
-  console.log('🚀 ~ file: MotionGallery.jsx:39 ~ handleOnPlaylistClick ~ selectedPlaylistId:', selectedPlaylistId)
-  setSelectedPlaylistId(selectedPlaylistId)
-  if (!selectedPlaylistId) {
-    setPlaylistVideos(videos.items)
+  function handleOnPlaylistClick(selectedPlaylistId) {
+    console.log('🚀 ~ file: MotionGallery.jsx:39 ~ handleOnPlaylistClick ~ selectedPlaylistId:', selectedPlaylistId)
+    setSelectedPlaylistId(selectedPlaylistId)
+    if (!selectedPlaylistId) {
+      setPlaylistVideos(videos.items)
+    }
   }
-}
-
 
   useEffect(() => {
-    console.log("🚀 ~ file: MotionGallery.jsx:43 ~ useEffect ~ videos:", videos)
+    console.log('🚀 ~ file: MotionGallery.jsx:43 ~ useEffect ~ videos:', videos)
   }, [videos])
 
   const router = useRouter()
 
+  useEffect(() => {
+    const parentDivs = document.querySelectorAll('.parentDiv')
+    const buttonDivs = document.querySelectorAll('.buttonDiv')
+    const mouseoverListeners = []
+    const mouseoutListeners = []
 
- useEffect(() => {
-   const parentDivs = document.querySelectorAll('.parentDiv')
-   const buttonDivs = document.querySelectorAll('.buttonDiv')
-   const mouseoverListeners = []
-   const mouseoutListeners = []
+    parentDivs.forEach((parentDiv, index) => {
+      const mouseover = () => {
+        buttonDivs[index].className =
+          'buttonDiv opacity-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex font-normal text-left '
+      }
+      const mouseout = () => {
+        buttonDivs[index].className =
+          'buttonDiv opacity-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex font-normal text-left '
+      }
 
-   parentDivs.forEach((parentDiv, index) => {
-     const mouseover = () => {
-       buttonDivs[index].className =
-         'buttonDiv opacity-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex font-normal text-left '
-     }
-     const mouseout = () => {
-       buttonDivs[index].className =
-         'buttonDiv opacity-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex font-normal text-left '
-     }
+      parentDiv.addEventListener('mouseover', mouseover)
+      parentDiv.addEventListener('mouseout', mouseout)
 
-     parentDiv.addEventListener('mouseover', mouseover)
-     parentDiv.addEventListener('mouseout', mouseout)
+      mouseoverListeners.push(mouseover)
+      mouseoutListeners.push(mouseout)
+    })
 
-     mouseoverListeners.push(mouseover)
-     mouseoutListeners.push(mouseout)
-   })
+    return () => {
+      parentDivs.forEach((parentDiv, index) => {
+        parentDiv.removeEventListener('mouseover', mouseoverListeners[index])
+        parentDiv.removeEventListener('mouseout', mouseoutListeners[index])
+      })
+    }
+  }, [])
 
-   return () => {
-     parentDivs.forEach((parentDiv, index) => {
-       parentDiv.removeEventListener('mouseover', mouseoverListeners[index])
-       parentDiv.removeEventListener('mouseout', mouseoutListeners[index])
-     })
-   }
- }, [])
-
-  
-  
   return (
     <>
-      <div className='z-10 flex-col flex-auto w-screen h-screen '>
-        <div className='relative flex-col justify-center flex-auto'>
-          <div className='h-1/5 '>
-            <ul className='flex pt-[7.5%] align-center justify-center'>
-              <li key="all">
-                <button
-                  className='text-[1.1vw] uppercase tracking-[3.68px] leading-relaxed pt-[10%] font-normal font-BrandonReg text-icewhite'
-                  onClick={() => handleOnPlaylistClick()}
-                >
-                  #all
-                </button>
-              </li>
+      <div className='z-10 flex w-screen h-screen '>
+        <div className='relative flex-col '>
+          <div className='relative flex '>
+            <div className='relative flex-col  mt-32 justify-center items-center align-middle ml-auto mr-auto'>
+              <p className='relative ml-3 text-left w-1/2 text-base uppercase tracking-[3.68px] leading-relaxed font-normal font-BrandonReg text-icewhite'>
+                FILTERS:
+              </p>
+              <ul className='flex w-full '>
+                <li key='all'>
+                  <button
+                    className='justify-center items-center w-full ml-3 mr-3 h-full text-left text-base uppercase tracking-[3.68px] leading-relaxed font-normal font-BrandonReg text-icewhite'
+                    onClick={() => handleOnPlaylistClick()}
+                  >
+                    #all{' '}
+                  </button>
+                </li>
 
-              {playlists &&
-                playlists.map((playlist) => {
-                  const isActive = playlist.id === activePlaylist
-                  // console.log('FOLDERSZ', playlists)
-                  return (
-                    <li key={playlist.id} data-active-folder={isActive} className='flex items-center justify-center'>
-                      <button
-                        className='w-full h-full text-center text-[1.1vw] align-center uppercase tracking-[3.68px] leading-relaxed pt-[10%] font-normal font-BrandonReg text-icewhite'
-                        onClick={() => handleOnPlaylistClick(playlist.id)}
-                      >
-                        {' '}
-                        #{playlist.snippet.localized.title}{' '}
-                      </button>
-                    </li>
-                  )
-                })}
-            </ul>
+                {playlists &&
+                  playlists.map((playlist) => {
+                    const isActive = playlist.id === activePlaylist
+                    // console.log('FOLDERSZ', playlists)
+                    return (
+                      <li key={playlist.id} data-active-folder={isActive}>
+                        <button
+                          className=' w-full h-full ml-3 mr-3 text-left text-base uppercase tracking-[3.68px] leading-relaxed font-normal font-BrandonReg text-icewhite'
+                          onClick={() => handleOnPlaylistClick(playlist.id)}
+                        >
+                          {' '}
+                          #{playlist.snippet.localized.title}{' '}
+                        </button>
+                      </li>
+                    )
+                  })}
+              </ul>
+            </div>
           </div>
 
-          <div className='grid justify-center w-screen grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4  h-3/4 overflow-auto left-11'>
+          <div className='relative grid w-screen align-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 h-2/3 m-6 overflow-auto'>
             {videos &&
               Array.isArray(videos) &&
               videos.map((video, index) => {
                 // console.log('🚀 ~ file: MotionGallery.jsx:79 ~ results:', videos)
+
+                let title = video.snippet.title.replace(/&AMP;/gi, '&')
+                title = title.replace(/&#39;/gi, "'")
+                console.log("🚀 ~ file: MotionGallery.jsx:130 ~ MotionGallery ~ title:", title)
+                
+
+
                 return (
-                  <div key={video.etag} className='parentDiv relative flex flex-col items-center justify-center'>
+                  <div key={video.etag} className='parentDiv relative flex flex-col items-center justify-center w-full'>
                     <Image
                       src={video.snippet.thumbnails.medium.url}
                       alt={video.snippet.title}
@@ -146,11 +154,11 @@ function handleOnPlaylistClick(selectedPlaylistId) {
                         toggleMotionPlayer()
                       }}
                     >
-                      <div className='w-12 rounded-full h-12'>
+                      <div className='w-12 rounded-full h-12 mt-4 m-auto'>
                         <svg
                           fill='#ffaf31'
-                          height='100px'
-                          width='100px'
+                          height='32px'
+                          width='32px'
                           version='1.1'
                           id='Layer_1'
                           xmlns='http://www.w3.org/2000/svg'
@@ -183,15 +191,15 @@ function handleOnPlaylistClick(selectedPlaylistId) {
                         </svg>
                       </div>
                     </button>
+                    <div className="w-full flex relative">
+                      <p className='relative text-base truncate uppercase tracking-[3.68px] leading-relaxed font-normal font-BrandonReg text-icewhite'>
+                        {title}
+                      </p>
+                    </div>
                   </div>
                 )
               })}
           </div>
-        </div>
-        <div>
-          <p>
-            <button>Load More Results</button>
-          </p>
         </div>
       </div>
     </>
